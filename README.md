0ff05ad5c368009245bdb7496db2600a590e2c5e
t0t0r <victormarechal59@gmail.com>
2023-06-20 09:32:08 +0200

    :rocket: fini

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ### HOW TO RUN
 
 ```
+git clone https://github.com/totor59/brief1.git
+cd brief1/
 chmod +x install.sh
 sudo ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 cd /tmp
-sudo apt install gcc curl linux-headers-$(uname -r)
+
+# requirements
+sudo apt install build-essential gcc curl linux-headers-$(uname -r)
+
 # install vscode
-command -v code >/dev/null 2>&1 || wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb && sudo dpkg -i vscode.deb
-# test vscode install
-command -v code arg.txt >/dev/null 2>&1 || { echo >&2 "/!\ Problem with vscode installation. Aborting."; exit 1; }
+wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb && sudo dpkg -i vscode.deb
 
 # install vmware workstation pro
-command -v vmware >/dev/null 2>&1 || curl -J -L https://www.vmware.com/go/getworkstation-linux -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" -o vmware.bundle && chmod +x vmware.bundle && sudo ./vmware.bundle
+curl -J -L https://www.vmware.com/go/getworkstation-linux -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" -o vmware.bundle && chmod +x vmware.bundle && sudo ./vmware.bundle
 
+# run vscode & vmware for test & post installation
+code arg.txt &
+vmware-installer &
+
+# clean
+rm /tmp/vscode.deb
+rm /tmp/vmware.bundle
